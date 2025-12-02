b8ec9db317d563aa76f1d97b945dc2df050805ae
Samki-bit <ssameep5@gmail.com>
2025-12-03 01:14:21 +0545

    sidebar skeleton

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,29 @@
+import Block from "../assets/block.svg";
+
+export default function Sidebar() {
+  const blocks = ["Block-1", "Block-2", "Block-3"];
+  return (
+    <div className="w-64 h-screen text-black flex flex-col p-4 gap-3 border-r-2 border-black/50">
+      <div className="text-2xl font-bold p-2">KU Smart Meter</div>
+      <div>
+        <div className="w-full text-left px-3 py-2 rounded-lg transition">Dashboard</div>
+        <div className="w-full text-left px-3 py-2 rounded-lg transition">Analysis</div>
+        <div className="w-full text-left px-3 py-2 rounded-lg  transition">Map</div>
+      </div>
+
+      <div>
+        <div className="py-2 px-1">Available Meters</div>
+        <div>
+          {blocks.length > 0 &&
+            blocks.map((block) => (
+              <div className="w-full text-left rounded-lg transition flex gap-3 px-3 py-2">
+                {" "}
+                <img src={Block} className="h-5 w-5" />
+                {block}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
